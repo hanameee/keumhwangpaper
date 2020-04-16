03d4b4ceca5a954ed322a8363b8c8a33a7a429c4
hanameee <hanameee@gmail.com>
2020-04-16 17:45:15 +0900

    Add modal function to CertificateModal

--- src/components/HoverableCard.js
+++ src/components/HoverableCard.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Paper } from "@material-ui/core";
 import Img from "gatsby-image";
 
-const HoverableCard = ({ image, hoverText }) => {
+const HoverableCard = ({ image, hoverText, modalClickHandler }) => {
     return (
         <div className="HoverableCard">
             <div className="CardImage">
                 <Paper elevation={3}>
                     <Img fluid={image} loading="eager" />
                 </Paper>
-                <div className="CardHoverText">
+                <div
+                    className="CardHoverText"
+                    onClick={() => modalClickHandler(true, image)}
+                >
                     <p>{hoverText}</p>
                 </div>
             </div>
